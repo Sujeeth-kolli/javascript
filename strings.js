c259cf99805c7812e707692bc56451cb035a2159
Sujeeth-kolli <kolli.sujeeth@gmail.com>
2025-06-28 18:34:27 +0530

    strings

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 //strings
-let firstName = "Sujeeth ";
-let secondName = "kolli";
+// let firstName = "Sujeeth ";
+// let secondName = "kolli";
 // 1. concatenation
 // let fullName= firstName.concat(secondName);
 // console.log(fullName)
@@ -17,7 +17,35 @@ let secondName = "kolli";
 // console.log(firstName.toUpperCase());
 
 //5. slice
-console.log(firstName.slice(1,6));
+// console.log(firstName.slice(1,6));
 
 //6. split & join 
+// console.log(firstName.split(" "));
+// console.log(firstName.split(" ").join(""));
+
+//7. includes
+// console.log(firstName.includes("S"));
+
+// 8. trim
+// console.log(firstName.trim());
+
+
+//1. create variable name (favActorFirstName) and store your favorite actor's first name in it
+//2. create variable name (favActorLastName) and store your favorite actor's last name in it
+//3. create variable name (FullName) and concatenate the first name and last name
+//4. create variable name (uppercase) & CAPITALIZE your fav actor name
+//5. Create a variable name (message) & store `my favorite Actor is (favActorName) & say something about your fav actor `name should be uppercase.
+//6. Now append this message to the (message variable) `his best show is Silicon Valley`
+//7. Now log your message.
+
+let favActorFirstName = 'chris';
+let favActorLastName = 'evans';
+let FullName = favActorFirstName.concat(favActorLastName);
+console.log(FullName);
+let upperCase = FullName.toUpperCase();
+console.log(upperCase);
+let message = `my favorite actor is ${upperCase} `;
+message += `he played captain america `;
+console.log(message);
+
 
