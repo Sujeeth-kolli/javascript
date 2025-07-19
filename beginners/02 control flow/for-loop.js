d8761afae7ecc29e8d376d776ebc9e11e5471e65
Sujeeth-kolli <kolli.sujeeth@gmail.com>
2025-07-19 10:54:50 +0530

    arrays

---
+++ beginners/02 control flow/for-loop.js
@@ -0,0 +1,15 @@
+// for (initialExpression; condition; incrementExpression) {}
+
+for (let i = 0; i <= 5; i++) {
+  console.log("Hello World", i);
+}
+
+// EXERCISE
+// ... (let ; = ; < ; ) {
+//   console.log(i);
+// }
+// Print your name & numbers from 0 through 1000
+
+for (let i = 0; i <= 1000; i++) {
+  console.log("HuXn WebDev", i);
+}
---
+++ beginners/02 control flow/while-loop.js
@@ -0,0 +1,21 @@
+// while (condition) {
+//   // code block to be executed
+// }
+
+// ----------------
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("Hello World", i);
+//   i++;
+// }
+// ----------------
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let i = 10;
+
+while (i <= 100) {
+  console.log("HuXn WebDev");
+  i++;
+}
